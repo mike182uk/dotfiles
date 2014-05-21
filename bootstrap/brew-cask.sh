ea1a08a72d216514bb05ee6c555222ac4de32ddd
Michael Barrett <mbarrett@inviqa.com>
2014-05-21 09:37:42 +0100

    add casks

--- bootstrap/brew-cask.sh
+++ bootstrap/brew-cask.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 brew cask install alfred
+brew cask install atom
 brew cask install dropbox
 brew cask install google-chrome
 brew cask install lastfm
 brew cask install mou
 brew cask install spotify
+brew cask install sublime-text
+brew cask install phpstorm
+brew cask install sourcetree
+brew cask install xtrafinder
+brew cask install utorrent
+brew cask install virtualbox
+brew cask install vagrant
 
 brew cask cleanup
