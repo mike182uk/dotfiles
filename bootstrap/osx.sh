c5545b05b43040f7a4317fc8db8f1d96532d4f90
Michael Barrett <mike182uk@gmail.com>
2014-08-21 20:15:11 +0100

    add hot corners

--- bootstrap/osx.sh
+++ bootstrap/osx.sh
@@ -214,6 +214,27 @@ defaults write com.apple.dock autohide -bool true
 # Reset Launchpad
 find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
 
+# Hot corners
+#
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+#
+# Top left screen corner → Desktop
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Bottom left screen corner → Desktop
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
 ###############################################################################
 # Terminal                                                                    #
 ###############################################################################
