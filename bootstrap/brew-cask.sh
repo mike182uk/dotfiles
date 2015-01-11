fbcdb10add53cafb4198f4a8f388f91aad6ac060
Michael Barrett <mike182uk@gmail.com>
2015-01-11 16:33:02 +0000

    update apps to be installed

--- bootstrap/brew-cask.sh
+++ bootstrap/brew-cask.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+brew cask install 1password
 brew cask install alfred
 brew cask install atom
+brew cask install chefdk
+brew cask install dash
 brew cask install dropbox
 brew cask install firefox
 brew cask install google-chrome
@@ -9,7 +12,9 @@ brew cask install iterm2
 brew cask install lastfm
 brew cask install mou
 brew cask install mysqlworkbench
+brew cask install omnifocus
 brew cask install sequel-pro
+brew cask install sonos
 brew cask install spectacle
 brew cask install spotify
 brew cask install sublime-text
@@ -17,8 +22,10 @@ brew cask install phpstorm
 brew cask install sourcetree
 brew cask install xtrafinder
 brew cask install utorrent
-brew cask install virtualbox
 brew cask install vagrant
+brew cask install virtualbox
+brew cask install vmware-fusion
 brew cask install vlc
+brew cask install webstrom
 
 brew cask cleanup
