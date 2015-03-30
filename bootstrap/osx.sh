5c458efd892925df1db63cd024cf2ad233fcab30
Michael Barrett <mbarrett@inviqa.com>
2015-03-30 09:38:13 +0100

    keep transparency

--- bootstrap/osx.sh
+++ bootstrap/osx.sh
@@ -14,7 +14,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo nvram SystemAudioVolume=" "
 
 # Disable transparency in the menu bar and elsewhere on Yosemite
-defaults write com.apple.universalaccess reduceTransparency -bool true
+#defaults write com.apple.universalaccess reduceTransparency -bool true
 
 # Set sidebar icon size to medium
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
