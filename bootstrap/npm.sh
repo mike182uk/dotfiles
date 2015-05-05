aa7b05c594f9b77bb8b9fcf2b0fe29866433d98f
Michael Barrett <mbarrett@inviqa.com>
2015-05-05 12:13:14 +0100

    add jscs

--- bootstrap/npm.sh
+++ bootstrap/npm.sh
@@ -5,10 +5,11 @@ npm update
 
 # install useful packages globally
 npm install -g bower
+npm install -g jscs
+npm install -g jshint
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g nodemon
-npm install -g jshint
 npm install -g yo
 
 # install yeoman generators
