f948a72aa6b687325a17d39408b37a954d503dec
Michael Barrett <mbarrett@inviqa.com>
2015-06-17 10:04:49 +0100

    add nvm bootstrap

---
+++ bootstrap/nvm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install latest stable version of node
+nvm install stable
+
+# Install latest stable version of iojs
+nvm install iojs
+
+# Use iojs as the default node version
+nvm alias default iojs
