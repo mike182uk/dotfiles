fbbbb1d8a95a1c347ee2a0ea66d576317cd68792
Michael Barrett <mbarrett@inviqa.com>
2015-11-30 10:30:43 +0000

    add useful node modules

--- bootstrap/npm.sh
+++ bootstrap/npm.sh
@@ -5,15 +5,18 @@ npm update
 
 # install useful packages globally
 npm install -g bower
+npm install -g complexity-report
 npm install -g jscs
 npm install -g jshint
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g node-inspector
 npm install -g nodemon
+npm install -g nsp
+npm install -g plato
+npm install -g retire
 npm install -g travis-encrypt
 npm install -g yo
 
 # install yeoman generators
 npm install -g generator-webapp
-npm install -g generator-angular
