935b26c1e99f4b2921a20bcc58e39246ceca5449
Michael Barrett <mbarrett@inviqa.com>
2016-06-14 10:56:46 +0100

    remove iojs

--- bootstrap/nvm.sh
+++ bootstrap/nvm.sh
@@ -3,8 +3,5 @@
 # Install latest stable version of node
 nvm install stable
 
-# Install latest stable version of iojs
-nvm install iojs
-
 # Use iojs as the default node version
 nvm alias default stable
