ec1a074774fb96cb6d0ede032eeba5ad8717dd76
Michael Barrett <mike182uk@gmail.com>
2016-08-18 09:17:09 +0100

    update ruby to 2.3.1

--- bootstrap/rbenv.sh
+++ bootstrap/rbenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install latest stable version of ruby
-rbenv install 2.2.2
+rbenv install 2.3.1
 
 # Use latest stable version of ruby globally
-rbenv global 2.2.2
+rbenv global 2.3.1
