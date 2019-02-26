fedb5630b17f3ad7cb4de5006f775e55e6d95eb5
Michael Barrett <mike182uk@gmail.com>
2019-02-26 14:39:40 +0000

    update spectacle installer

--- spectacle/install.sh
+++ spectacle/install.sh
@@ -5,4 +5,4 @@ set -e
 # https://stackoverflow.com/a/246128/1004917
 CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)
 
-cp -r "$CURRENT_DIR/Spectacle.json" ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
+cp -r "$CURRENT_DIR/Spectacle.json" ~/Library/Application\ Support/Spectacle/Shortcuts.json 2>/dev/null
