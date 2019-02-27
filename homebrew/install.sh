db6461fb214f9fcff9f59a7279f5153fe0b22df2
Michael Barrett <mike182uk@gmail.com>
2019-02-27 15:48:31 +0000

    update brew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,6 +5,9 @@ set -e
 # https://stackoverflow.com/a/246128/1004917
 CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)
 
+# shellcheck source=/dev/null
+. "$CURRENT_DIR/env.zsh"
+
 if test "$(command -v brew)"; then
 	brew bundle --file="$CURRENT_DIR/Brewfile"
 fi
