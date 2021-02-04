79f36d9e6fae8e7660da142a06ae3c496878f9fb
Michael Barrett <mike182uk@gmail.com>
2021-02-04 21:31:40 +0000

    install pure manually

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,9 +3,13 @@
 set -e
 
 LOCAL_RC_PATH=$HOME/.localrc
+PLUGINS_PATH=$HOME/.zsh
 
 if [ -f "$LOCAL_RC_PATH" ]; then
 	mv "$LOCAL_RC_PATH" "$LOCAL_RC_PATH.backup$(date +%s)"
 fi
 
 touch "$LOCAL_RC_PATH"
+
+# install plugins
+git clone https://github.com/sindresorhus/pure.git "$PLUGINS_PATH/pure"
---
+++ zsh/prompt.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+autoload -U promptinit; promptinit
+
+zstyle :prompt:pure:git:stash show yes
+
+prompt pure
