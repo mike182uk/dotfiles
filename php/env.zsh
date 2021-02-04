cd4fe15c745cc8d307fcd7f32d71edd05713ff66
Michael Barrett <mike182uk@gmail.com>
2021-02-04 22:28:15 +0000

    move php env var

--- php/env.zsh
+++ php/env.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 # initialise phpenv
+export PHP_BUILD_CONFIGURE_OPTS="--with-bz2=$(brew --prefix bzip2) --with-iconv=$(brew --prefix libiconv) --with-tidy=$(brew --prefix tidy-html5) --with-zlib-dir=$(brew --prefix zlib)"
+
 if test "$(command -v phpenv)"; then
 	eval "$(phpenv init -)"
 fi
