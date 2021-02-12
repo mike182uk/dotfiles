87cf4cd70383f147c3ccf82eb1bd27af9aba54ed
Michael Barrett <mike182uk@gmail.com>
2021-02-12 22:37:30 +0000

    use fnm instead of nvm

--- node/env.zsh
+++ node/env.zsh
@@ -6,13 +6,7 @@ export NODE_REPL_HISTORY_FILE=$HOME/.node_history
 # allow 32³ entries; the default is 1000
 export NODE_REPL_HISTORY_SIZE=32768
 
-# nvm dir
-export NVM_DIR=$HOME/.nvm
-
-# lazy load nvm
-nvm() {
-  # shellcheck source=/dev/null
-  . "$NVM_DIR/nvm.sh"
-
-  nvm "$@"
-}
+if test "$(command -v fnm)"; then
+  # eval "$(fnm env)"
+  _evalcache fnm env
+fi
--- node/install.sh
+++ node/install.sh
@@ -2,26 +2,8 @@
 
 set -e
 
-# shellcheck source=/dev/null
-. "$(dirname "$0")/env.zsh"
-
-# ensure nvm is installed
-if [ ! -s "$NVM_DIR/nvm.sh" ]; then
-  if [ -d "$NVM_DIR" ]; then
-    mv "$NVM_DIR" "$NVM_DIR.backup$(date +%s)"
-  fi
-
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-
-  # shellcheck source=/dev/null
-  . "$NVM_DIR/nvm.sh"
-fi
-
-if test "$(command -v nvm)"; then
-  nvm install stable
-  nvm alias default stable
-
-  echo "you may need to reload your shell if you can not access node or npm"
+if test "$(command -v fnm)"; then
+  fnm install --lts
 fi
 
 # ensure empty node history file has been created
